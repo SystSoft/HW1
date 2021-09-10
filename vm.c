e24db687195644d6220c828fa2abb362596f5989
jenniennie <90437083+jenniennie@users.noreply.github.com>
2021-09-10 00:35:13 -0400

    Create vm.c

---
+++ vm.c
@@ -0,0 +1,44 @@
+/* Fall 2021 COP 3402 HW1 */
+/* Anjaly Davis, Jennifer Nguyen */
+/* September 12rd, 2021 */
+
+#include "tester.sh"
+#include <stdio.h>
+
+// Global Veriables
+#define 
+
+void print_execution(int line, char *opname, int *IR, int PC, int BP, int SP, int DP, int *pas, int GP)
+{
+    int i;
+    // print out instruction and registers
+    printf("%2d\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t", line, opname, IR[1], IR[2], PC, BP, SP, DP);
+    
+    // print data section
+    for (i = GP; i <= DP; i++)
+        printf("%d ", pas[i]);
+    
+    printf("\n");
+    
+    // print stack
+    printf("\tstack : ");
+    
+    for (i = MAX_PAS_LENGTH - 1; i >= SP; i--)
+        printf("%d ", pas[i]);printf("\n");
+}
+
+int base(int L)
+{
+    int arb = BP;   // arb = activation record base
+    while ( L > 0)     //find base L levels down
+    {
+        arb = pas[arb];
+        L--;
+    }
+    return arb;
+}
+
+int main(int argc, char **argv)
+{
+    
+}
